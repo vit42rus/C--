69cbc3dcb7d806317abaa7af609451a727fc3fab
Vit <vit_2282@mail.ru>
2022-09-09 16:20:43 +0700

    3 задание

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,1 +1,54 @@
 ﻿
+        // задача 1
+
+
+// Console.Write("Введите пятизначное число: ");
+// string number = Console.ReadLine();
+
+// void CheckingNumber(string number){
+//   if (number[0]==number[4] && number[1]==number[3]){
+//     Console.WriteLine($"это число: {number} - палиндром.");
+//   }
+//   else Console.WriteLine($"это: {number} - НЕ палиндром.");
+// }
+
+// if (number!.Length == 5){
+//   CheckingNumber(number);
+// }
+
+
+              //  Задача 2
+
+
+
+
+// System.Console.WriteLine("Введите координату x первой точки : ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите координату y первой точки : ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите координату z первой точки : ");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+
+
+// System.Console.WriteLine("Введите координату x второй точки : ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите координату y второй точки : ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите координату z второй точки : ");
+// int z2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+// double rez= Math.Sqrt(Math.Pow ((x2-x1),2) + Math.Pow ((y2-y1),2) + Math.Pow ((z2-z1),2 ));
+// System.Console.WriteLine(rez);
+
+
+                        // Задача 3
+                        
+                        
+// Console.Write("Введите число :  ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// for (int i =1 ;i <= n; i++){
+//         System.Console.WriteLine(i*i*i);
+// }
