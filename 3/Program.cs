326fd4f52126211e4cddb8fda68f04c2674b1476
Vit <vit_2282@mail.ru>
2022-09-10 11:23:55 +0700

    дополнил решения

--- 3/Program.cs
+++ 3/Program.cs
@@ -17,6 +17,31 @@
 // }
 
 
+// System.Console.WriteLine("Введите число : ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int x1 = n / 10000;
+// n -= x1 * 10000;
+
+// int x2 = n / 1000;
+// n -= x2 * 1000;
+
+// int x3 = n / 100;
+// n -= x3 * 100;
+
+// int x4 = n / 10;
+// n -= x4 * 10;
+
+// int x5 = n % 10;
+
+// if (x1 == x5 && x2 == x4){
+//         System.Console.WriteLine("это палиндром");
+// }
+// else{
+//         System.Console.WriteLine("не палиндром");
+// }
+
+
               //  Задача 2
 
 
@@ -50,5 +75,7 @@
 // int n = Convert.ToInt32(Console.ReadLine());
 
 // for (int i =1 ;i <= n; i++){
-//         System.Console.WriteLine(i*i*i);
+        // System.Console.WriteLine(i*i*i);  
+                 // или
+                 // System.Console.WriteLine(Math.Pow(i,3));
 // }
