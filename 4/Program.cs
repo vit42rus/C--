a1a8dfe9ef138b1f57ae1217838382dc6b3ddf07
Vit <vit_2282@mail.ru>
2022-09-16 17:52:44 +0700

    4 задание

---
+++ 4/Program.cs
@@ -0,0 +1,52 @@
+﻿                // 1 Задача
+
+// Console.Write("введите число : ");
+// int i = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите степень : ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+//    System.Console.WriteLine(Math.Pow(i,n));
+
+
+
+                //  2 Задача
+
+
+// Console.Write("Введите число : ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+//   int Sum(int n){
+    
+//     int counter = Convert.ToString(n).Length;
+//     int advance = 0;
+//     int result = 0;
+
+//     for (int i = 0; i < counter; i++){
+//       advance = n - n % 10;
+//       result = result + (n - advance);
+//       n = n / 10;
+//     }
+//    return result;
+//   }
+
+// int sum = Sum(n);
+// Console.WriteLine("Сумма цифр в числе: " + sum);
+
+
+            // 3 Задача
+
+int [] numbers = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(0, 11);
+    Console.Write(" " + Method (i) + " ");
+ }
+Console.Write("]");
+
+int Method (int a)
+{
+    return numbers[a];
+}
